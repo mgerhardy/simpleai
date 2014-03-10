eb089725eb5f0ea3b5d361232b132e2322667179
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-10 21:26:04 +0100

    * removed parameter from HasEnemies

--- src/example/behaviours.lua
+++ src/example/behaviours.lua
@@ -1,5 +1,5 @@
 function init ()
 	local tree = AI.createTree("walk")
 	local rootNode = tree:createRoot("PrioritySelector", "root");
-	rootNode:addNode("Move", "move"):setCondition("Not(HasEnemies{2})")
+	rootNode:addNode("Move", "move"):setCondition("Not(HasEnemies)")
 end
