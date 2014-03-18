80809f729ee55afcb3ffdf35e23b32d0819a95ef
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-18 16:08:50 +0100

    * only evaluate the selector state once

--- src/ai/tree/Selector.h
+++ src/ai/tree/Selector.h
@@ -25,8 +25,9 @@ public:
 
 	void getChildrenState(const AI& entity, std::vector<bool>& active) const override {
 		int n = 0;
+		int selectorState = getSelectorState(entity);
 		for (TreeNodes::const_iterator i = _children.begin(); i != _children.end(); ++i, ++n)
-			active.push_back(getSelectorState(entity) == n);
+			active.push_back(selectorState == n);
 	}
 };
 
