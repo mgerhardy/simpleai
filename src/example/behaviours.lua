3bb01e34a33371d61a82a68ee619f160edddc919
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-18 18:48:51 +0100

    * extended example behaviour tree

--- src/example/behaviours.lua
+++ src/example/behaviours.lua
@@ -1,6 +1,8 @@
 function init ()
 	local tree = AI.createTree("walk")
 	local rootNode = tree:createRoot("PrioritySelector", "root");
+	rootNode:addNode("Idle{1000}", "idle1000")
 	rootNode:addNode("Move", "move"):setCondition("Not(HasEnemies)")
-	rootNode:addNode("Idle{1000}", "idle")
+	local prioSelector = rootNode:addNode("PrioritySelector", "foobar");
+	prioSelector:addNode("Idle{10}", "idle10")
 end
