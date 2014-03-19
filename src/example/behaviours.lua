f3e3c3c2c7612852e0557fab18c0e53d18fc0976
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-19 09:01:20 +0100

    * use a sequence for the example

--- src/example/behaviours.lua
+++ src/example/behaviours.lua
@@ -1,8 +1,8 @@
 function init ()
 	local tree = AI.createTree("walk")
-	local rootNode = tree:createRoot("PrioritySelector", "root");
-	rootNode:addNode("Idle{1000}", "idle1000")
+	local rootNode = tree:createRoot("Sequence", "root");
+	rootNode:addNode("Idle{500}", "idle500")
 	rootNode:addNode("Move", "move"):setCondition("Not(HasEnemies)")
-	local prioSelector = rootNode:addNode("PrioritySelector", "foobar");
+	local prioSelector = rootNode:addNode("Parallel", "foobar");
 	prioSelector:addNode("Idle{10}", "idle10")
 end
