04eb62d28afbf4639d0122ca1e1ec1c0143492a1
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-24 21:21:44 +0100

    * updated behaviour tree for testing

--- src/example/behaviours.lua
+++ src/example/behaviours.lua
@@ -1,8 +1,10 @@
 function init ()
 	local tree = AI.createTree("example")
 	local rootNode = tree:createRoot("Sequence", "root");
-	rootNode:addNode("Idle{500}", "idle500")
+	rootNode:addNode("Idle{3000}", "idle3000")
+	local prioSelector = rootNode:addNode("PrioritySelector", "prio2");
+	prioSelector:addNode("Idle{3000}", "idle3000"):setCondition("HasEnemies")
+	prioSelector:addNode("Idle{1000}", "idle1000")
+	rootNode:addNode("Idle{3000}", "idle3000")
 	rootNode:addNode("Move", "move"):setCondition("Not(HasEnemies)")
-	local prioSelector = rootNode:addNode("Parallel", "foobar");
-	prioSelector:addNode("Idle{10}", "idle10")
 end
