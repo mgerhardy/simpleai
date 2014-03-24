06254e30cfcb437996cc8af37774c57e0c18ec59
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-24 21:07:11 +0100

    * made getChildrenState in the Selector class virtual

--- src/ai/tree/Selector.h
+++ src/ai/tree/Selector.h
@@ -20,7 +20,7 @@ class Selector: public TreeNode {
 public:
 	NODE_CLASS(Selector);
 
-	void getChildrenState(const AI& entity, std::vector<bool>& active) const override {
+	virtual void getChildrenState(const AI& entity, std::vector<bool>& active) const override {
 		int n = 0;
 		int selectorState = getSelectorState(entity);
 		for (TreeNodes::const_iterator i = _children.begin(); i != _children.end(); ++i, ++n)
