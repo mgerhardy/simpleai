de23cca650c840c159a7f9114276e634c09e0fff
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-25 17:49:37 +0100

    * removed output in class PauseHandler

--- src/ai/server/PauseHandler.cpp
+++ src/ai/server/PauseHandler.cpp
@@ -1,13 +1,11 @@
 #include "PauseHandler.h"
 #include "Server.h"
-#include <iostream>
 
 namespace ai {
 
 void PauseHandler::execute(const ClientId& clientId, const IProtocolMessage& message) override {
 	const AIPauseMessage& msg = static_cast<const AIPauseMessage&>(message);
 	_server.pause(clientId, msg.isPause());
-	std::cout << "PAUSE" << std::endl;
 }
 
 }
