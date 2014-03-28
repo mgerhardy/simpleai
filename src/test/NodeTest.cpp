aeec4397b63b9ebae2dd3107dcb50fa722a492a1
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-03-28 09:01:19 +0100

    * extended sequence test

--- src/test/NodeTest.cpp
+++ src/test/NodeTest.cpp
@@ -7,7 +7,35 @@ TEST_F(NodeTest, testSequence) {
 	ai::Sequence::Factory f;
 	ai::TreeNodeFactoryContext ctx("testsequence", "", ai::True::get());
 	ai::TreeNodePtr node = f.create(&ctx);
-	//node->addChild();
+
+	ai::Idle::Factory idleFac;
+	ai::TreeNodeFactoryContext idleCtx1("testidle", "2", ai::True::get());
+	ai::TreeNodePtr idle1 = idleFac.create(&idleCtx1);
+	ai::TreeNodeFactoryContext idleCtx2("testidle2", "2", ai::True::get());
+	ai::TreeNodePtr idle2 = idleFac.create(&idleCtx2);
+
+	node->addChild(idle1);
+	node->addChild(idle2);
+
+	TestEntity e(1, node, _pathfinder);
+	e.update(1);
+	ASSERT_EQ(ai::RUNNING, idle1->getLastStatus());
+	ASSERT_EQ(ai::UNKNOWN, idle2->getLastStatus());
+	e.update(1);
+	ASSERT_EQ(ai::RUNNING, idle1->getLastStatus());
+	ASSERT_EQ(ai::UNKNOWN, idle2->getLastStatus());
+	e.update(1);
+	ASSERT_EQ(ai::FINISHED, idle1->getLastStatus());
+	ASSERT_EQ(ai::RUNNING, idle2->getLastStatus());
+	e.update(1);
+	ASSERT_EQ(ai::FINISHED, idle1->getLastStatus());
+	ASSERT_EQ(ai::RUNNING, idle2->getLastStatus());
+	e.update(1);
+	ASSERT_EQ(ai::FINISHED, idle1->getLastStatus());
+	ASSERT_EQ(ai::FINISHED, idle2->getLastStatus());
+	e.update(1);
+	ASSERT_EQ(ai::RUNNING, idle1->getLastStatus());
+	ASSERT_EQ(ai::FINISHED, idle2->getLastStatus());
 }
 
 TEST_F(NodeTest, testIdle) {
