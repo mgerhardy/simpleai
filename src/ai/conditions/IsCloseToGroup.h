956bf7aad4d6e13dafa7ddf4afdee8bdd1843903
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-04-02 18:30:54 +0200

    * doxygen for IsCloseToGroup

--- src/ai/conditions/IsCloseToGroup.h
+++ src/ai/conditions/IsCloseToGroup.h
@@ -6,6 +6,12 @@
 
 namespace ai {
 
+/**
+ * @brief Checks whether the controlled @c AI is close to a particular group.
+ *
+ * The parameters are given as the group id and the distance to the group that
+ * triggers this condition to evaluate to @c true.
+ */
 class IsCloseToGroup: public ICondition {
 private:
 	GroupId _groupId;
