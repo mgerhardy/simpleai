bd1b77aff6437cab0b0c835ff96d329c7fc26a3c
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-04-19 12:50:58 +0200

    * doxygen

--- src/ai/server/AIStubTypes.h
+++ src/ai/server/AIStubTypes.h
@@ -6,6 +6,11 @@
 
 namespace ai {
 
+/**
+ * @brief The aggro entry for the @c AIStateAggro
+ *
+ * Holds a character id and the assigned aggro value
+ */
 struct AIStateAggroEntry {
 	AIStateAggroEntry(ai::CharacterId _id, float _aggro) :
 			id(_id), aggro(_aggro) {
@@ -14,6 +19,9 @@ struct AIStateAggroEntry {
 	float aggro;
 };
 
+/**
+ * @brief The list of aggro entry for a character
+ */
 class AIStateAggro {
 private:
 	std::vector<AIStateAggroEntry> _aggro;
@@ -27,6 +35,9 @@ public:
 	}
 };
 
+/**
+ * @brief This is a representation of a behaviour tree node for the serialization
+ */
 class AIStateNode {
 private:
 	std::string _name;
@@ -61,37 +72,61 @@ public:
 		return _condition;
 	}
 
+	/**
+	 * @return The milliseconds of the last execution of this particular node
+	 */
 	inline int64_t getLastRun() const {
 		return _lastRun;
 	}
 
+	/**
+	 * @return The @c TreeNodeStatus of the last execution
+	 */
 	inline TreeNodeStatus getStatus() const {
 		return _status;
 	}
 
+	/**
+	 * @brief Some nodes have a state that holds which children was the last active one.
+	 * @return Whether this particular node is active
+	 */
 	inline bool isActive() const {
 		return _active;
 	}
 };
 
+/**
+ * @brief This is a representation of a character state for the serialization
+ */
 class AIStateWorld {
 private:
 	ai::CharacterId _id;
 	ai::Vector3f _position;
 	float _orientation;
 public:
-	AIStateWorld(const ai::CharacterId& id, const ai::Vector3f& position, float orientation) :
+	AIStateWorld(const ai::CharacterId& id, const ai::Vector3f& position, float orientation = -1.0f) :
 			_id(id), _position(position), _orientation(orientation) {
 	}
 
+	/**
+	 * @return The unique id that can be used to identify the character in the world
+	 */
 	inline const ai::CharacterId& getId() const {
 		return _id;
 	}
 
+	/**
+	 * @return The orientation of the character [0, 2*PI]
+	 *
+	 * @note A negative value means, that the character does not have any orientation
+	 */
 	inline float getOrientation() const {
 		return _orientation;
 	}
 
+	/**
+	 * @return The position in the world
+	 */
 	inline const ai::Vector3f& getPosition() const {
 		return _position;
 	}
