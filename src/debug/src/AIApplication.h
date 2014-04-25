9d80a1d4e23cbea1e2287afa6ed535e90f652aca
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-04-25 08:56:13 +0200

    * doxygen comments

--- src/debug/src/AIApplication.h
+++ src/debug/src/AIApplication.h
@@ -9,6 +9,12 @@ namespace debug {
 class AIDebugger;
 class AIDebuggerWidget;
 
+/**
+ * @brief Use this class to run the debugger as a stand-alone application.
+ *
+ * @note If you embed the debugger into an already exsting QT application, you of course
+ * don't need this.
+ */
 class AIApplication: public QApplication {
 protected:
 	AIDebugger* _debugger;
--- src/debug/src/gui/AIDebuggerWidget.h
+++ src/debug/src/gui/AIDebuggerWidget.h
@@ -18,6 +18,9 @@ class ZoomFrame;
 class MapView;
 class AIDebugger;
 
+/**
+ * @brief The widget that represents the whole ai debugger
+ */
 class AIDebuggerWidget: public QWidget {
 Q_OBJECT
 public:
@@ -26,14 +29,38 @@ public:
 
 	void connectToAIServer(const QString& hostname, short port);
 
+	/**
+	 * @brief Call this with an existing @c QStatusBar to add additional ai debugger information to it
+	 */
 	void contributeToStatusBar(QStatusBar* statusBar);
+	/**
+	 * @brief Call this with an existing @c QToolbar to add addition ai debugger buttons to it
+	 */
 	void contributeToToolBar(QToolBar* toolBar);
+	/**
+	 * @brief Call this with an existing @c QToolbar to add addition ai debugger entries to it
+	 */
 	void contributeToFileMenu(QMenu *fileMenu);
+	/**
+	 * @brief Call this with an existing @c QToolbar to add addition ai debugger entries to it
+	 */
 	void contributeToHelpMenu(QMenu *helpMenu);
 
+	/**
+	 * @brief If you let the ai debugger contribute to the status bar, call if to remove the contribution
+	 */
 	void removeFromStatusBar(QStatusBar* statusBar);
+	/**
+	 * @brief If you let the ai debugger contribute to the tool bar, call if to remove the contribution
+	 */
 	void removeFromToolBar(QToolBar* toolBar);
+	/**
+	 * @brief If you let the ai debugger contribute to the file menu, call if to remove the contribution
+	 */
 	void removeFromFileMenu(QMenu *fileMenu);
+	/**
+	 * @brief If you let the ai debugger contribute to the help menu, call if to remove the contribution
+	 */
 	void removeFromHelpMenu(QMenu *helpMenu);
 
 	void tick();
