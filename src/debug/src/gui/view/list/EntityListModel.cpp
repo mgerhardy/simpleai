8453cb84cb38614542540ff811a61ed9d988a419
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-05-06 15:12:20 +0200

    * removed row selecting and scrolling for model resets - not needed

--- src/debug/src/gui/view/list/EntityListModel.cpp
+++ src/debug/src/gui/view/list/EntityListModel.cpp
@@ -12,25 +12,8 @@ EntityListModel::~EntityListModel() {
 }
 
 void EntityListModel::update() {
-	QModelIndexList selectedList = _parent->selectionModel()->selectedRows();
-	QModelIndex scrollTopIndex = _parent->indexAt(QPoint(1, 1));
-	QModelIndex scrollBottomIndex = _parent->indexAt(QPoint(1, _parent->height() - 1));
-
 	beginResetModel();
 	endResetModel();
-
-	if (scrollBottomIndex.row() == -1) {
-		_parent->scrollToBottom();
-	} else {
-		_parent->scrollTo(scrollTopIndex);
-	}
-
-	if (selectedList.count() > 0
-			&& (selectedList.at(0).row() >= scrollTopIndex.row())
-			&& (scrollBottomIndex.row() == -1
-					|| scrollBottomIndex.row() > selectedList.at(0).row())) {
-		_parent->selectRow(selectedList.at(0).row());
-	}
 }
 
 int EntityListModel::rowCount(const QModelIndex & /*parent*/) const {
