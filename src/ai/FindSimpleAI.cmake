7abcab720288eaecbc7dcfe26d60465eb2a58965
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-11-28 10:29:49 +0100

    * fixed cmake find package script

--- src/ai/FindSimpleAI.cmake
+++ src/ai/FindSimpleAI.cmake
@@ -1,18 +1,28 @@
+# - Try to find SimpleAI
+# Once done this will define
+#  SIMPLEAI_FOUND - System has SimpleAI
+#  SIMPLEAI_INCLUDE_DIRS - The SimpleAI include directories
+#  SIMPLEAI_LIBRARIES - The libraries needed to use SimpleAI
+#  SIMPLEAI_DEFINITIONS - Compiler switches required for using SimpleAI
+
+find_package(PkgConfig)
+pkg_check_modules(PC_SIMPLEAI QUIET libsimpleai)
+set(SIMPLEAI_DEFINITIONS ${PC_SIMPLEAI_CFLAGS_OTHER})
+
 find_path(SIMPLEAI_INCLUDE_DIR SimpleAI.h
-  HINTS
-    ENV SIMPLEAIDIR
-  PATH_SUFFIXES include/simpleai include
-)
+          HINTS ${PC_SIMPLEAI_INCLUDEDIR} ${PC_SIMPLEAI_INCLUDE_DIRS}
+          PATH_SUFFIXES simpleai )
 
-find_library(SIMPLEAI_LIBRARY
-  NAMES simpleai
-  HINTS
-    ENV SIMPLEAIDIR
-  PATH_SUFFIXES lib
-)
+find_library(SIMPLEAI_LIBRARY NAMES simpleai
+             HINTS ${PC_SIMPLEAI_LIBDIR} ${PC_SIMPLEAI_LIBRARY_DIRS} )
 
-set(SIMPLEAI_LIBRARIES ${SIMPLEAI_LIBRARY})
-set(SIMPLEAI_INCLUDE_DIRS ${SIMPLEAI_INCLUDE_DIR})
-include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(simpleai REQUIRED_VARS SIMPLEAI_LIBRARIES SIMPLEAI_INCLUDE_DIRS)
-mark_as_advanced(SIMPLEAI_LIBRARY SIMPLEAI_INCLUDE_DIR)
+set(SIMPLEAI_LIBRARIES ${SIMPLEAI_LIBRARY} )
+set(SIMPLEAI_INCLUDE_DIRS ${SIMPLEAI_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set SIMPLEAI_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(SimpleAI  DEFAULT_MSG
+                                  SIMPLEAI_LIBRARY SIMPLEAI_INCLUDE_DIR)
+
+mark_as_advanced(SIMPLEAI_INCLUDE_DIR SIMPLEAI_LIBRARY )
