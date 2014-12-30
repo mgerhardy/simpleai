dee3ea26f99bf318fc756c5e43732725a3664eae
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-12-30 12:40:10 +0100

    * add more attributes

--- src/run/GameEntity.cpp
+++ src/run/GameEntity.cpp
@@ -26,6 +26,9 @@ void GameEntity::update(long deltaTime) {
 
 	// update attributes for debugging
 	setAttribute(ai::attributes::POSITION, std::to_string(getPosition()));
+	setAttribute("Hitpoints", std::to_string(getHitpoints()));
+	setAttribute("Reloadtime", std::to_string(getAttackDelay()));
+	setAttribute("Damage", std::to_string(getDamage()));
 	setAttribute(ai::attributes::SPEED, std::to_string(getSpeed()));
 	setAttribute(ai::attributes::ORIENTATION, std::to_string(toDegrees(getOrientation())));
 }
