c56c9a07fbd4787349468935b798ba61fb20f018
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-07 09:41:10 +0100

    * minor cleanup

--- src/ai/common/Thread.h
+++ src/ai/common/Thread.h
@@ -1,6 +1,15 @@
 #pragma once
 
-#ifndef AI_NO_THREADING
+#ifdef AI_NO_THREADING
+
+#define MUTEXTYPE
+#define MUTEX(name)
+#define LOCK
+#define SCOPEDLOCK(mutex)
+#define ATOMIC(type) type
+#define ATOMICINT int
+
+#else
 
 #include <thread>
 #include <mutex>
@@ -10,16 +19,8 @@
 #define MUTEX(name) mutable MUTEXTYPE name
 #define LOCK std::lock_guard<MUTEXTYPE>
 #define SCOPEDLOCK(mutex) std::lock_guard<MUTEXTYPE> __lock__(mutex)
+// TODO: use memory_order_release for store and memory_order_consume for load
 #define ATOMIC(type) std::atomic<type>
 #define ATOMICINT std::atomic_int
-
-#else
-
-#define MUTEXTYPE
-#define MUTEX(name)
-#define LOCK
-#define SCOPEDLOCK(mutex)
-#define ATOMIC(type) type
-#define ATOMICINT int
 
 #endif
