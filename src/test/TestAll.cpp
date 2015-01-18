2d494db0be0a892811ce383fbdea176be4f8ea86
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-18 20:28:07 +0100

    * fixed tests if you don't compile with exception support

--- src/test/TestAll.cpp
+++ src/test/TestAll.cpp
@@ -17,10 +17,15 @@ extern "C" int main (int argc, char **argv)
 	//::testing::GTEST_FLAG(throw_on_failure) = true;
 	::testing::InitGoogleTest(&argc, argv);
 
+#if AI_EXCEPTIONS
 	try {
+#endif
 		return RUN_ALL_TESTS();
+
+#if AI_EXCEPTIONS
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
+#endif
 }
