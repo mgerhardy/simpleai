371f213328c0125ebede442ab791ae6f6fef7778
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-19 17:56:12 +0100

    * failing loader tests

--- src/test/LUATreeLoaderTest.cpp
+++ src/test/LUATreeLoaderTest.cpp
@@ -1,8 +1,11 @@
 #include "LUATreeLoaderTest.h"
-#include "tree/loaders/lua/LUATreeLoader.h"
+#include <SimpleAI.h>
 
 class LUATreeLoaderTest: public TestSuite {
 };
 
 TEST_F(LUATreeLoaderTest, testLoad) {
+	ai::AIRegistry registry;
+	ai::LUATreeLoader loader(registry);
+	ASSERT_TRUE(loader.init(""));
 }
--- src/test/XMLTreeLoaderTest.cpp
+++ src/test/XMLTreeLoaderTest.cpp
@@ -1,8 +1,11 @@
 #include "XMLTreeLoaderTest.h"
-#include "tree/loaders/xml/XMLTreeLoader.h"
+#include <SimpleAI.h>
 
 class XMLTreeLoaderTest: public TestSuite {
 };
 
 TEST_F(XMLTreeLoaderTest, testLoad) {
+	ai::AIRegistry registry;
+	ai::XMLTreeLoader loader(registry);
+	ASSERT_TRUE(loader.init(""));
 }
