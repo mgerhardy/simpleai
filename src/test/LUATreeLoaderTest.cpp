1735b1e8cccf919d70d14d1292bb6c70c2f5e44a
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-19 18:01:20 +0100

    * fixed lua tree loader test

--- src/test/LUATreeLoaderTest.cpp
+++ src/test/LUATreeLoaderTest.cpp
@@ -1,11 +1,18 @@
 #include "LUATreeLoaderTest.h"
 #include <SimpleAI.h>
 
+namespace {
+const char *TREE = "function init ()"
+		"local example = AI.createTree(\"example\")"
+		"local rootNodeExample = example:createRoot(\"PrioritySelector\", \"root\")"
+		"end";
+}
+
 class LUATreeLoaderTest: public TestSuite {
 };
 
 TEST_F(LUATreeLoaderTest, testLoad) {
 	ai::AIRegistry registry;
 	ai::LUATreeLoader loader(registry);
-	ASSERT_TRUE(loader.init(""));
+	ASSERT_TRUE(loader.init(TREE));
 }
--- src/test/XMLTreeLoaderTest.cpp
+++ src/test/XMLTreeLoaderTest.cpp
@@ -1,11 +1,15 @@
 #include "XMLTreeLoaderTest.h"
 #include <SimpleAI.h>
 
+namespace {
+const char *TREE = "<?xml version=\"1.0\" standalone=\"no\" ?>";
+}
+
 class XMLTreeLoaderTest: public TestSuite {
 };
 
 TEST_F(XMLTreeLoaderTest, testLoad) {
 	ai::AIRegistry registry;
 	ai::XMLTreeLoader loader(registry);
-	ASSERT_TRUE(loader.init(""));
+	ASSERT_TRUE(loader.init(TREE));
 }
