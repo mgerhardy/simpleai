c166e1fd160690d53f66566d7eda0082357a9f27
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-19 18:45:09 +0100

    * extended lua tree node parser test

--- src/test/LUATreeLoaderTest.cpp
+++ src/test/LUATreeLoaderTest.cpp
@@ -5,6 +5,7 @@ namespace {
 const char *TREE = "function init ()"
 		"local example = AI.createTree(\"example\")"
 		"local rootNodeExample = example:createRoot(\"PrioritySelector\", \"root\")"
+		"rootNodeExample:addNode(\"Idle{3000}\", \"idle3000\"):setCondition(\"True\")"
 		"end";
 }
 
@@ -15,4 +16,12 @@ TEST_F(LUATreeLoaderTest, testLoad) {
 	ai::AIRegistry registry;
 	ai::LUATreeLoader loader(registry);
 	ASSERT_TRUE(loader.init(TREE)) << loader.getError();
+	const ai::TreeNodePtr& tree = loader.load("example");
+	ASSERT_NE(nullptr, tree.get()) << "Could not find the espected behaviour";
+	ASSERT_EQ("root", tree->getName()) << "unexpected root node name";
+	const ai::TreeNodes& children = tree->getChildren();
+	const int childrenAmount = children.size();
+	ASSERT_EQ(1, childrenAmount) << "expected amount of children";
+	ASSERT_EQ("idle3000", children[0]->getName()) << "unexpected child node name";
+	ASSERT_EQ("True", children[0]->getCondition()->getName()) << "unexpected condition name";
 }
