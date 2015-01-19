b9392e1d9774e4ab19ed6529d65781c23a82f2f9
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-19 18:08:15 +0100

    * fixed xml tree loader test

--- src/ai/tree/loaders/xml/XMLTreeLoader.cpp
+++ src/ai/tree/loaders/xml/XMLTreeLoader.cpp
@@ -12,11 +12,24 @@ XMLTreeLoader::XMLTreeLoader(const IAIFactory& aiFactory) :
 bool XMLTreeLoader::init(const std::string& xmlData) {
 	tinyxml2::XMLDocument doc(false);
 	const int status = doc.Parse(xmlData.c_str());
-	tinyxml2::XMLElement* rootNode = doc.FirstChildElement( "behaviours" );
+	tinyxml2::XMLElement* rootNode = doc.FirstChildElement("behaviours");
 	if (rootNode == nullptr)
 		return false;
 	for (tinyxml2::XMLNode* node = rootNode->FirstChild(); node; node = node->NextSibling()) {
-		std::cout << node->ToElement()->Name() << std::endl;
+		tinyxml2::XMLElement* e = node->ToElement();
+		if (e == nullptr) {
+			_error = "unexpected node type";
+			return false;
+		}
+		if (!::strcmp("node", e->Name())) {
+			_error = "unexpected node name";
+			return false;
+		}
+		const char *type = e->Attribute("type");
+		if (type == nullptr) {
+			_error = "node has no type attribute";
+			return false;
+		}
 	}
 	/** @todo implement parsing */
 	return status == tinyxml2::XML_NO_ERROR;
