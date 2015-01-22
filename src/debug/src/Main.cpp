c011728706da5a72e483426df31c44f1ca67ba7b
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-22 17:52:22 +0100

    * profiler support for debugger

--- src/debug/src/Main.cpp
+++ src/debug/src/Main.cpp
@@ -2,7 +2,15 @@
 #include "AIDebugger.h"
 #include "AIDebuggerWidget.h"
 
+#ifdef AI_PROFILER
+#include <google/profiler.h>
+#endif
+
 int main(int argc, char **argv) {
+#ifdef AI_PROFILER
+	ProfilerStart("simpleai-debugger.prof");
+#endif
+
 	ai::debug::AIApplication app(argc, argv);
 	return app.exec();
 }
