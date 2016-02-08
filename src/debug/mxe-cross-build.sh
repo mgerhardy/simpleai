9cb2028751f2bacc7e609078bc19b8c40f1bb040
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-02-08 17:34:50 +0100

    * cross compile - fixed toolchain target name

--- src/debug/mxe-cross-build.sh
+++ src/debug/mxe-cross-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MXE_PATH=${1:-~/dev/mxe}
-TARGET=${2:-i686-w64-mingw32}
+TARGET=${2:-i686-w64-mingw32.static}
 JOBS=${3:-4}
 
 PATH="${MXE_PATH}"/usr/bin/:$PATH QMAKE_CXX="ccache g++" ${MXE_PATH}/usr/${TARGET}/qt5/bin/qmake
