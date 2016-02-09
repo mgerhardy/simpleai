c9300715d6a4b266cbd91b1d38f5ac14af60764b
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-02-09 22:22:18 +0100

    * script: build for clang debug mode

--- cmake.sh
+++ cmake.sh
@@ -6,8 +6,11 @@ DIR=$(dirname $(readlink -f $0))
 
 JOBS=4
 
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
 cd $DIR/..
 mkdir -p simpleai-build
 cd simpleai-build
-cmake -DSIMPLEAI_DEBUGGER=ON -DCMAKE_BUILD_TYPE=Release $DIR $*
+cmake -DSIMPLEAI_DEBUGGER=ON -DCMAKE_BUILD_TYPE=Debug $DIR $*
 make -j ${JOBS} $*
