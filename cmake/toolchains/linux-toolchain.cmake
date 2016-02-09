bb7690e5bee2afcc3c0345c9d2c8c1889389267d
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-02-09 21:55:57 +0100

    * cmake: more sanitizers supported

--- cmake/toolchains/linux-toolchain.cmake
+++ cmake/toolchains/linux-toolchain.cmake
@@ -6,17 +6,33 @@ set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 find_package(Threads)
 
 set(CMAKE_REQUIRED_FLAGS "-Werror -fsanitize=undefined")
-check_c_compiler_flag("-faddress-sanitizer" HAVE_FLAG_SANITIZE_UNDEFINED)
+check_c_compiler_flag("-fsanitize=undefined" HAVE_FLAG_SANITIZE_UNDEFINED)
 set(CMAKE_REQUIRED_FLAGS "-Werror -fsanitize=address")
 check_c_compiler_flag("-fsanitize=address" HAVE_FLAG_SANITIZE_ADDRESS)
+set(CMAKE_REQUIRED_FLAGS "-Werror -fsanitize=thread")
+check_c_compiler_flag("-fsanitize=thread" HAVE_FLAG_SANITIZE_THREAD)
+set(CMAKE_REQUIRED_FLAGS "-Werror -fsanitize=leak")
+check_c_compiler_flag("-fsanitize=leak" HAVE_FLAG_SANITIZE_LEAK)
 unset(CMAKE_REQUIRED_FLAGS)
 
 if (HAVE_FLAG_SANITIZE_UNDEFINED)
 	set(SANITIZE_FLAGS "${SANITIZE_FLAGS} -fsanitize=undefined")
+	message("Support undefined sanitizer")
+endif()
+
+if (HAVE_FLAG_SANITIZE_LEAK)
+	set(SANITIZE_FLAGS "${SANITIZE_FLAGS} -fsanitize=leak")
+	message("Support leak sanitizer")
+endif()
+
+if (HAVE_FLAG_SANITIZE_THREAD)
+	set(SANITIZE_FLAGS "${SANITIZE_FLAGS} -fsanitize=thread")
+	message("Support thread sanitizer")
 endif()
 
 if (HAVE_FLAG_SANITIZE_ADDRESS)
 	set(SANITIZE_FLAGS "${SANITIZE_FLAGS} -fsanitize=address")
+	message("Support address sanitizer")
 endif()
 
 
