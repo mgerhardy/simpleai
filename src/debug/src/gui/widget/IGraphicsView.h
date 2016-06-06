91d1d4f90b59d886b28efe7f8f096873362081f5
mgerhardy <martin.gerhardy@gmail.com>
2016-06-06 19:53:18 +0200

    * added Q_PROPERTY to IGraphicsView

--- src/debug/src/gui/widget/IGraphicsView.h
+++ src/debug/src/gui/widget/IGraphicsView.h
@@ -7,11 +7,28 @@ namespace debug {
 
 class IGraphicsView : public QGraphicsView {
 private:
+	Q_PROPERTY(bool renderGrid READ renderGrid WRITE setRenderGrid)
+	Q_PROPERTY(bool renderBackground READ renderBackground WRITE setRenderBackground)
+
 	bool _renderGrid;
 	bool _renderBackground;
 public:
 	IGraphicsView(bool renderGrid = false, bool renderBackground = false, QWidget* parent = nullptr);
 	virtual ~IGraphicsView();
+
+	bool renderGrid() const {
+		return _renderGrid;
+	}
+	bool renderBackground() const {
+		return _renderBackground;
+	}
+
+	void setRenderGrid(bool renderGrid) {
+		_renderGrid = renderGrid;
+	}
+	void renderBackground(bool renderBackground) {
+		_renderBackground = renderBackground;
+	}
 
 	virtual void drawBackground(QPainter* painter, const QRectF& rect) override;
 };
