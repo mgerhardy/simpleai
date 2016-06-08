4bddd2b38df8e33ac754ccbca59254d5d1c96f01
mgerhardy <martin.gerhardy@gmail.com>
2016-06-08 17:09:43 +0200

    * updated cmake script

--- cmake.sh
+++ cmake.sh
@@ -20,3 +20,9 @@ mkdir -p simpleai-build-debug
 cd simpleai-build-debug
 cmake -DSIMPLEAI_DEBUGGER=ON -DCMAKE_BUILD_TYPE=Debug -G"Eclipse CDT4 - Unix Makefiles" $DIR
 make -j ${JOBS} $*
+
+cd $DIR/..
+mkdir -p simpleai-build-clang-analyser
+cd simpleai-build-clang-analyser
+scan-build-3.9 cmake -DSIMPLEAI_DEBUGGER=ON -DSIMPLEAI_USE_CCACHE=OFF $DIR
+scan-build-3.9 make -j ${JOBS} $*
