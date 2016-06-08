bfb48c0d9e6105d4d1e4bbbe5f8c968bd28ff824
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-06-08 20:18:27 +0200

    * removed std::cout call

--- src/ai/server/AddNodeHandler.h
+++ src/ai/server/AddNodeHandler.h
@@ -16,8 +16,9 @@ public:
 
 	void execute(const ClientId& /*clientId*/, const IProtocolMessage& message) override {
 		const AIAddNodeMessage& msg = static_cast<const AIAddNodeMessage&>(message);
-		if (!_server.addNode(msg.getCharacterId(), msg.getParentNodeId(), msg.getName(), msg.getType(), msg.getCondition()))
-			std::cout << "Failed to add the new node" << std::endl;
+		if (!_server.addNode(msg.getCharacterId(), msg.getParentNodeId(), msg.getName(), msg.getType(), msg.getCondition())) {
+			ai_log_error("Failed to add the new node");
+		}
 	}
 };
 
