8c6b7263587238793239fe03f118badd0b656a97
Documentation Builder <martin.gerhardy@gmail.com>
2016-10-13 22:56:56 +0200

    * extended tests to allow to rerun filters, conditions and tasks

--- src/test/LUAAIRegistryTest.cpp
+++ src/test/LUAAIRegistryTest.cpp
@@ -51,39 +51,45 @@ protected:
 		_registry.shutdown();
 	}
 
-	void testFilter(const char* filterName) {
+	void testFilter(const char* filterName, int n = 1) {
 		SCOPED_TRACE(filterName);
 		const ai::FilterPtr& filter = _registry.createFilter(filterName, ctxFilter);
 		ASSERT_TRUE((bool)filter) << "Could not create lua provided filter";
 		const ai::AIPtr& ai = std::make_shared<ai::AI>(ai::TreeNodePtr());
 		ai->setCharacter(_chr);
-		filter->filter(ai);
+		for (int i = 0; i < n; ++i) {
+			filter->filter(ai);
+		}
 		ASSERT_EQ(1, filter.use_count()) << "Someone is still referencing the LUAFilter";
 	}
 
-	void testCondition(const char* conditionName, bool expectedReturnValue) {
+	void testCondition(const char* conditionName, bool expectedReturnValue, int n = 1) {
 		SCOPED_TRACE(conditionName);
 		const ai::ConditionPtr& condition = _registry.createCondition(conditionName, ctxCondition);
 		ASSERT_TRUE((bool)condition) << "Could not create lua provided condition";
 		const ai::AIPtr& ai = std::make_shared<ai::AI>(ai::TreeNodePtr());
 		ai->setCharacter(_chr);
-		ASSERT_EQ(expectedReturnValue, condition->evaluate(ai));
+		for (int i = 0; i < n; ++i) {
+			ASSERT_EQ(expectedReturnValue, condition->evaluate(ai));
+		}
 		ASSERT_EQ(1, condition.use_count()) << "Someone is still referencing the LUACondition";
 	}
 
-	void testNode(const char* nodeName, ai::TreeNodeStatus status) {
+	void testNode(const char* nodeName, ai::TreeNodeStatus status, int n = 1) {
 		const ai::TreeNodeFactoryContext ctx = ai::TreeNodeFactoryContext("TreeNodeName", "", ai::True::get());
-		return testNode(nodeName, status, ctx);
+		return testNode(nodeName, status, ctx, n);
 	}
 
-	void testNode(const char* nodeName, ai::TreeNodeStatus status, const ai::TreeNodeFactoryContext &ctx) {
+	void testNode(const char* nodeName, ai::TreeNodeStatus status, const ai::TreeNodeFactoryContext &ctx, int n = 1) {
 		SCOPED_TRACE(nodeName);
 		const ai::TreeNodePtr& node = _registry.createNode(nodeName, ctx);
 		ASSERT_TRUE((bool)node) << "Could not create lua provided node '" << nodeName << "'";
 		const ai::AIPtr& ai = std::make_shared<ai::AI>(node);
 		ai->setCharacter(_chr);
-		const ai::TreeNodeStatus executionStatus = node->execute(ai, 1L);
-		ASSERT_EQ(status, executionStatus) << "Lua script returned an unexpected TreeNodeStatus value";
+		for (int i = 0; i < n; ++i) {
+			const ai::TreeNodeStatus executionStatus = node->execute(ai, 1L);
+			ASSERT_EQ(status, executionStatus) << "Lua script returned an unexpected TreeNodeStatus value";
+		}
 		ai->setBehaviour(ai::TreeNodePtr());
 		ASSERT_EQ(1, node.use_count()) << "Someone is still referencing the LUATreeNode";
 		ASSERT_EQ(1, ai.use_count()) << "Someone is still referencing the AI instance";
@@ -98,6 +104,10 @@ TEST_F(LUAAIRegistryTest, testLuaNode2) {
 	testNode("LuaTest2", ai::TreeNodeStatus::RUNNING);
 }
 
+TEST_F(LUAAIRegistryTest, testLuaNode2_100) {
+	testNode("LuaTest2", ai::TreeNodeStatus::RUNNING, 100);
+}
+
 TEST_F(LUAAIRegistryTest, testCreateInvalidNode) {
 	const ai::TreeNodeFactoryContext ctx = ai::TreeNodeFactoryContext("TreeNodeName", "", ai::True::get());
 	const ai::TreeNodePtr& node = _registry.createNode("ThisNameDoesNotExist", ctx);
@@ -108,10 +118,18 @@ TEST_F(LUAAIRegistryTest, testConditionEvaluationTrue) {
 	testCondition("LuaTestTrue", true);
 }
 
+TEST_F(LUAAIRegistryTest, testConditionEvaluationTrue_100) {
+	testCondition("LuaTestTrue", true, 100);
+}
+
 TEST_F(LUAAIRegistryTest, testConditionEvaluationFalse) {
 	testCondition("LuaTestFalse", false);
 }
 
 TEST_F(LUAAIRegistryTest, testFilterEmpty) {
 	testFilter("LuaFilterTest");
+}
+
+TEST_F(LUAAIRegistryTest, testFilter_100) {
+	testFilter("LuaFilterTest", 100);
 }
