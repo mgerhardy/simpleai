5bdec8e3f00ffbd513b157f7835caa7d7a530f01
Documentation Builder <martin.gerhardy@gmail.com>
2016-10-18 12:16:36 +0000

    Automated documentation build for changeset 6eff7f6c368353f9120689481010b83dc0c8e2e5.

--- search/files_8.js
+++ search/files_8.js
@@ -4,10 +4,8 @@ var searchData=
   ['limit_2eh',['Limit.h',['../Limit_8h.html',1,'']]],
   ['log_2eh',['Log.h',['../Log_8h.html',1,'']]],
   ['lua_2eh',['LUA.h',['../LUA_8h.html',1,'']]],
-  ['luaairegistry_2eh',['LUAAIRegistry.h',['../LUAAIRegistry_8h.html',1,'']]],
   ['luacondition_2eh',['LUACondition.h',['../LUACondition_8h.html',1,'']]],
   ['luafilter_2eh',['LUAFilter.h',['../LUAFilter_8h.html',1,'']]],
-  ['luafunctions_2eh',['LUAFunctions.h',['../LUAFunctions_8h.html',1,'']]],
   ['luasteering_2eh',['LUASteering.h',['../LUASteering_8h.html',1,'']]],
   ['luatreeloader_2eh',['LUATreeLoader.h',['../LUATreeLoader_8h.html',1,'']]],
   ['luatreenode_2eh',['LUATreeNode.h',['../LUATreeNode_8h.html',1,'']]]
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['load',['load',['../classai_1_1ITreeLoader.html#a4be204a94bab15c16bcd25c854837003',1,'ai::ITreeLoader']]],
-  ['luaai_5fcreatenode',['luaAI_createnode',['../classai_1_1LUAAIRegistry.html#a20fa73fb35d2ea301c46e90c6389dcfb',1,'ai::LUAAIRegistry']]]
+  ['load',['load',['../classai_1_1ITreeLoader.html#a4be204a94bab15c16bcd25c854837003',1,'ai::ITreeLoader']]]
 ];
