d8f17c42976d3b2df14d7f1dea087e140dd44981
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-04 19:04:57 +0100

    * extended ThreadScheduler test

--- src/test/ThreadTest.cpp
+++ src/test/ThreadTest.cpp
@@ -6,12 +6,16 @@ class ThreadTest: public TestSuite {
 TEST_F(ThreadTest, testScheduleAtFixedRate) {
 	ai::ThreadScheduler scheduler;
 	std::atomic_int countExecution(0);
-	scheduler.scheduleAtFixedRate(std::chrono::milliseconds(0), std::chrono::milliseconds(10), [&] () {
+	const int delay = 10;
+	const int waitTime = 200;
+	scheduler.scheduleAtFixedRate(std::chrono::milliseconds(0), std::chrono::milliseconds(delay), [&] () {
 		++countExecution;
 	});
-	std::this_thread::sleep_for(std::chrono::milliseconds(200));
-	ASSERT_GE(countExecution, 10);
-	ASSERT_LE(countExecution, 30);
+	std::this_thread::sleep_for(std::chrono::milliseconds(waitTime));
+	const int delta = 3;
+	const int expected = waitTime / delay;
+	ASSERT_GE(countExecution, expected - delta);
+	ASSERT_LE(countExecution, expected + delta);
 }
 
 TEST_F(ThreadTest, testSchedule) {
@@ -22,4 +26,28 @@ TEST_F(ThreadTest, testSchedule) {
 	});
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	ASSERT_EQ(1, countExecutionOnce);
+}
+
+TEST_F(ThreadTest, testScheduleInitialDelay) {
+	ai::ThreadScheduler scheduler;
+	std::atomic_int countExecutionOnce(0);
+	scheduler.schedule(std::chrono::milliseconds(20), [&] () {
+		++countExecutionOnce;
+	});
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	ASSERT_EQ(0, countExecutionOnce);
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	ASSERT_EQ(1, countExecutionOnce);
+}
+
+TEST_F(ThreadTest, testScheduleAbortBeforeExecute) {
+	ai::ThreadScheduler scheduler;
+	std::atomic_int countExecutionOnce(0);
+	const int timerId = scheduler.schedule(std::chrono::milliseconds(20), [&] () {
+		++countExecutionOnce;
+	});
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	ASSERT_TRUE(scheduler.cancel(timerId));
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	ASSERT_EQ(0, countExecutionOnce);
 }
