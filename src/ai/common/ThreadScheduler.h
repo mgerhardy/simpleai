mgerhardy/simpleai

commit 30f25398a75ced3508e443db730b43d0f7883502

* reworked the thread scheduler

src/ai/common/ThreadScheduler.h
@@ -4,45 +4,41 @@
 #pragma once
 
 #include <chrono>
-#include <vector>
+#include <set>
 #include <functional>
-#include <queue>
 #include <thread>
 #include <future>
 #include <atomic>
 #include <mutex>
-#include <condition_variable>
 
 namespace ai {
 
 /**
- * Schedules tasks that executed with a fixed delay between their executions.
+ * Schedules tasks that are executed with a fixed delay between their executions and/or
+ * with a delay before their initial execution.
+ *
+ * @note There is a single thread that checks for scheduled tasks
  */
 class ThreadScheduler {
 private:
 	struct ScheduledTask {
 		ThreadScheduler* _scheduler;
 		std::function<void()> _callback;
+		// if this is > 0 the task is rescheduled
 		std::chrono::milliseconds _delay;
 		std::chrono::milliseconds _execTime;
+		int _timerId;
 
-		ScheduledTask(ThreadScheduler* scheduler, std::function<void()>&& callback, const std::chrono::milliseconds& initialDelay, const std::chrono::milliseconds& delay) :
-				_scheduler(scheduler), _callback(callback), _delay(delay), _execTime(initialDelay) {
+		ScheduledTask(ThreadScheduler* scheduler, std::function<void()>&& callback, const std::chrono::milliseconds& initialDelay, const std::chrono::milliseconds& delay, int timerId) :
+				_scheduler(scheduler), _callback(callback), _delay(delay), _execTime(initialDelay), _timerId(timerId) {
 		}
 
-		ScheduledTask(ThreadScheduler* scheduler, const std::function<void()>& callback, const std::chrono::milliseconds& initialDelay, const std::chrono::milliseconds& delay) :
-				_scheduler(scheduler), _callback(callback), _delay(delay), _execTime(initialDelay) {
-		}
-
-		void operator()() const {
+		/**
+		 * @return @c true if the task should get rescheduled
+		 */
+		bool operator()() const {
 			_callback();
-			if (_delay.count() <= 0) {
-				return;
-			}
-			// reschedule with the same delay from that moment the callback finished
-			auto epoch = std::chrono::system_clock::now().time_since_epoch();
-			auto now = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-			_scheduler->_tasks.emplace(_scheduler, _callback, now + _delay, _delay);
+			return _delay.count() > 0;
 		}
 
 		inline bool operator<(const ScheduledTask& other) const {
@@ -50,10 +46,11 @@ private:
 		}
 	};
 
-	std::priority_queue<ScheduledTask, std::vector<ScheduledTask> > _tasks;
+	std::set<ScheduledTask> _tasks;
+	std::atomic_int _timerId {0};
 	std::atomic_bool _stop;
 	std::atomic_bool _notEmpty;
-	std::mutex _queueMutex;
+	std::mutex _mutex;
 	std::thread _thread;
 
 public:
@@ -66,14 +63,23 @@ public:
 				if (this->_notEmpty) {
 					auto epoch = std::chrono::system_clock::now().time_since_epoch();
 					auto now = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-					std::unique_lock<std::mutex> lock(_queueMutex);
-					while (!this->_tasks.empty() && this->_tasks.top()._execTime <= now) {
+					std::unique_lock<std::mutex> lock(_mutex);
+					while (!this->_tasks.empty() && this->_tasks.begin()->_execTime <= now) {
 						if (this->_stop) {
 							return;
 						}
-						//std::async(std::launch::async, this->_tasks.top());
-						this->_tasks.top()();
-						this->_tasks.pop();
+						auto iter = this->_tasks.begin();
+						const ScheduledTask& t = *iter;
+						if (t()) {
+							auto callback = t._callback;
+							auto delay = t._delay;
+							auto timerId = t._timerId;
+							auto execTime = t._execTime;
+							this->_tasks.erase(iter);
+							_tasks.emplace(this, std::move(callback), execTime + delay, delay, timerId);
+						} else {
+							this->_tasks.erase(iter);
+						}
 					}
 					if (this->_stop) {
 						return;
@@ -93,24 +99,50 @@ public:
 		_thread.join();
 	}
 
+	/**
+	 * @brief Executes the given functor once after the given delay has passed.
+	 * @param[in] f The task to execute
+	 * @param[in] delay The period in milliseconds that must have been passed for the execution
+	 * @return The timer id
+	 */
 	template<class F, class ... Args>
-	void schedule(const std::chrono::milliseconds& delay, F&& f, Args&&... args) {
-		scheduleAtFixedRate(delay, std::chrono::milliseconds(), std::forward<F>(f), std::forward<Args>(args)...);
+	inline int schedule(const std::chrono::milliseconds& delay, F&& f, Args&&... args) {
+		return scheduleAtFixedRate(delay, std::chrono::milliseconds(), std::forward<F>(f), std::forward<Args>(args)...);
 	}
 
 	/**
+	 * @brief Schedules a task for continuous execution
 	 * @param[in] f The task to execute
 	 * @param[in] initialDelay The milliseconds to delay the first execution
-	 * @param[in] period The period in milliseconds between successive executions
+	 * @param[in] delay The period in milliseconds between successive executions
+	 * @return The timer id
 	 */
 	template<class F, class ... Args>
-	void scheduleAtFixedRate(const std::chrono::milliseconds& initialDelay, const std::chrono::milliseconds& delay, F&& f, Args&&... args) {
+	int scheduleAtFixedRate(const std::chrono::milliseconds& initialDelay, const std::chrono::milliseconds& delay, F&& f, Args&&... args) {
 		auto task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
 		auto epoch = std::chrono::system_clock::now().time_since_epoch();
 		auto now = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-		std::unique_lock<std::mutex> lock(_queueMutex);
-		_tasks.emplace(this, std::move<std::function<void()> >(task), now + initialDelay, delay);
+		const int timerId = ++_timerId;
+		std::unique_lock<std::mutex> lock(_mutex);
+		_tasks.emplace(this, std::move(task), now + initialDelay, delay, timerId);
 		_notEmpty = true;
+		return timerId;
+	}
+
+	/**
+	 * @brief Cancels the given timer
+	 * @param[in] timerId The timer id returned by schedule() or scheduleAtFixedRate()
+	 * @return @c true if the timer was canceled, @c false otherwise
+	 */
+	bool cancel(int timerId) {
+		std::unique_lock<std::mutex> lock(_mutex);
+		auto it = std::find_if(_tasks.begin(), _tasks.end(), [=] (const ScheduledTask& task) { return task._timerId == timerId; });
+		if (it == _tasks.end()) {
+			return false;
+		};
+		_tasks.erase(it);
+		return true;
 	}
 };
+
 }

commit 3bc177f1de27978af2420d24baaaecf6606b87fa

* fixed ThreadScheduler issues introduced in 30f25398

src/ai/common/ThreadScheduler.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include <chrono>
-#include <set>
+#include <queue>
 #include <functional>
 #include <thread>
 #include <future>
@@ -46,7 +46,18 @@ private:
 		}
 	};
 
-	std::set<ScheduledTask> _tasks;
+	class prioqueue: public std::priority_queue<ScheduledTask> {
+	public:
+		bool remove(int timerId) {
+			auto it = std::find_if(this->c.begin(), this->c.end(), [=] (const ScheduledTask& task) { return task._timerId == timerId; });
+			if (it == this->c.end()) {
+				return false;
+			};
+			this->c.erase(it);
+			return true;
+		}
+	};
+	prioqueue _tasks;
 	std::atomic_int _timerId {0};
 	std::atomic_bool _stop;
 	std::atomic_bool _notEmpty;
@@ -64,21 +75,20 @@ public:
 					auto epoch = std::chrono::system_clock::now().time_since_epoch();
 					auto now = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
 					std::unique_lock<std::mutex> lock(_mutex);
-					while (!this->_tasks.empty() && this->_tasks.begin()->_execTime <= now) {
+					while (!this->_tasks.empty() && this->_tasks.top()._execTime <= now) {
 						if (this->_stop) {
 							return;
 						}
-						auto iter = this->_tasks.begin();
-						const ScheduledTask& t = *iter;
+						const ScheduledTask& t = this->_tasks.top();
 						if (t()) {
 							auto callback = t._callback;
 							auto delay = t._delay;
 							auto timerId = t._timerId;
 							auto execTime = t._execTime;
-							this->_tasks.erase(iter);
+							this->_tasks.pop();
 							_tasks.emplace(this, std::move(callback), execTime + delay, delay, timerId);
 						} else {
-							this->_tasks.erase(iter);
+							this->_tasks.pop();
 						}
 					}
 					if (this->_stop) {
@@ -136,12 +146,7 @@ public:
 	 */
 	bool cancel(int timerId) {
 		std::unique_lock<std::mutex> lock(_mutex);
-		auto it = std::find_if(_tasks.begin(), _tasks.end(), [=] (const ScheduledTask& task) { return task._timerId == timerId; });
-		if (it == _tasks.end()) {
-			return false;
-		};
-		_tasks.erase(it);
-		return true;
+		return _tasks.remove(timerId);
 	}
 };
 